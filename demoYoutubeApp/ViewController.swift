8ba994de7b5451b2b33d65ebf031b9946e794431
haydenjin <haydenjin@gmail.com>
2020-07-09 22:37:01 -0600

    Initial xcode project creation

---
+++ demoYoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoYoutubeApp
+//
+//  Created by Hayden jin on 2020-07-09.
+//  Copyright © 2020 Hayden jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
